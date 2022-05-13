80a6183e681f8a613a93e513a5593e0c9be233b5
Björn Höfling <bjoern.hoefling@camptocamp.com>
2022-05-13 14:15:31 +0200

    From Archetype.

---
+++ src/main/java/com/camptocamp/bhoefling/App.java
@@ -0,0 +1,13 @@
+package com.camptocamp.bhoefling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
